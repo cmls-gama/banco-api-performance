440d3f1b1899d8d56480cf969cffea30dd5bb1c3
Caio Marques <cmls.marques@gmail.com>
2026-06-23 13:19:07 -0300

    Adicionando stages no código da aplicação de login

--- tests/login.test.js
+++ tests/login.test.js
@@ -2,8 +2,12 @@ import http from 'k6/http';
 import { sleep, check } from 'k6';
 
 export const options = {
-  vus: 10,
-  duration: '30s',
+  stages:[
+    {duration: '10s', target:10}, // durante 10segundos coloque 10 usuários virtuais para executar meus testes
+    {duration: '20s', target:10}, 
+    {duration: '10s', target:30},//durante mais 10s coloque 30 usuários virtuais
+    {duration: '20s', target:0},//nos 20 segundos finais devem zerar os usuários virtuais
+  ],
   thresholds: {
     http_req_duration: ['p(90)<3000','max<5000'], //percentil 90 TEM QUE SER menor que 10 milisegundos e o max menor que 1
     http_req_failed: ['rate<0.01'] 
